32add034ce87d6275fc3c99b1220b287f6d9edc0
sahil <sahil.ghatage22@pccoepune.org>
2026-01-12 11:19:00 +0530

    homepage react

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
+import './assets/css/style.css';
+import Header from './components/header';
+import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
-  <>
-  
-  </>
+    return (
+
+        <div className="app">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
 }
 
-export default App
+export default App;
